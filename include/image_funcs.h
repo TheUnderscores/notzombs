ae2f7faeae4c3da95778d4a09d38553eb2ff8656
JoshuaBrockschmidt <JoshuaBrockschmidt@gmail.com>
2015-02-14 20:43:11 -0800

    fixed image optimized image loading

--- include/image_funcs.h
+++ include/image_funcs.h
@@ -1,7 +1,15 @@
 #ifndef IMAGE_FUNCS_H
 #define IMAGE_FUNCS_H
 
-SDL_Surface *loadSurface(char *filepath)
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
+/* Loads a surface and optimizes it for a particular reference surface
+ * This was, SDL won't have to format the surface every time it blits it to the
+ * aforementioned reference surface
+ * @param optForSurf  Surface to optimized image that will be loaded for
+ * @param filepath  File path of image to be loaded and optimized
+ */
+SDL_Surface *loadSurface(SDL_Surface *optForSurf, char *filepath);
 
 #endif
---
+++ src/client/image_funcs.c
@@ -0,0 +1,30 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "image_funcs.h"
+
+SDL_Surface *loadSurface(SDL_Surface *optForSurf, char *filepath)
+{
+	SDL_Surface *loadedSurf = NULL;
+	/* Optimized surface */
+	SDL_Surface *optSurf = NULL;
+	
+	SDL_Surface *loadSurf = IMG_Load(filepath);
+	if (loadSurf == NULL) {
+		printf("Unable to load image %s! SDL_image Error:",
+		       filepath,
+		       IMG_GetError());
+		return NULL;
+	}
+	
+	optSurf = SDL_ConvertSurface(loadedSurf, optForSurf->format, NULL);
+	if (optSurf == NULL) {
+		printf("Unable to optimize image %s! SDL_image Error:",
+		       filepath,
+		       IMG_GetError());
+		return NULL;
+	}
+	
+	SDL_FreeSurface(loadedSurf);
+	
+	return optSurf;
+}
